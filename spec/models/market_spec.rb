61eb238b34e7d27b2d951ab3620fb3815ea52db6
Persa Zula <persa@persazula.com>
2014-01-27 19:40:36 -0700

    WIP: Adds Market/Address relationship spec -- failing

--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe Market do
+  describe "address association" do
+    it "should have an address" do
+      market = FactoryGirl.create(:market)
+      address = FactoryGirl.create(:address, :market_id => market.id)
+      expect(market.address.zipcode).to eq(44313)
+    end
+  end
 end
