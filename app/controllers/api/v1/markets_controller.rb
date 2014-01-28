aa75e40de6b07e18b2826d93137fac3c78512ddc
edatrix <lespath@gmail.com>
2014-01-28 14:09:20 -0700

    creates Market with_addresses method

--- app/controllers/api/v1/markets_controller.rb
+++ app/controllers/api/v1/markets_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::MarketsController < ApplicationController
 
   def index
     if params[:address]
-      markets = Market.includes(:address).all.as_json(:include => :address)
+      markets = Market.with_addresses.as_json(:include => :address)
     else
       markets = Market.all
     end
--- app/models/market.rb
+++ app/models/market.rb
@@ -2,4 +2,8 @@ class Market < ActiveRecord::Base
 
   has_one :address
 
+  def self.with_addresses
+    Market.includes(:address).all
+  end
+
 end
