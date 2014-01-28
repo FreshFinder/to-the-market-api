f7c0b3e432a7303bacaeeb3d67915d87702aa668
edatrix <lespath@gmail.com>
2014-01-28 13:54:41 -0700

    updates markets controller only to show addresses when flagged

--- app/controllers/api/v1/markets_controller.rb
+++ app/controllers/api/v1/markets_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::MarketsController < ApplicationController
 
   def index
-    markets = Market.includes(:address).all.as_json(:include => :address)
+    if params[:address]
+      markets = Market.includes(:address).all.as_json(:include => :address)
+    else
+      markets = Market.all
+    end
     render json: markets
   end
 end
