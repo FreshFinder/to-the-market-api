8ad7b8190adf317a57973744004a6e2e49b7413b
Simon Taranto <simon.taranto@gmail.com>
2014-02-08 15:30:50 -0700

    clean up some notes from refactoring

--- app/models/market_search_service.rb
+++ app/models/market_search_service.rb
@@ -1,7 +1,6 @@
 class MarketSearchService < ActiveRecord::Base
 
   def self.by_zipcode(target_zipcode, radius = 30)
-    # :TODO: clean up query
     Address.near(target_zipcode.to_s, radius).map(&:market)
   end
 
