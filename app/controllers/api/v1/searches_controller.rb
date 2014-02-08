d61c5a5a02efa6f5c801818fe49fc37ea3d94f03
edatrix <lespath@gmail.com>
2014-02-08 12:44:55 -0700

    finishes plumbing

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::SearchesController < ApplicationController
 
   def markets
+    markets = Market.all
+    render json: markets
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ ToTheMarket::Application.routes.draw do
   get "products/app/controllers/api/v1"
 
   namespace :api do
-    namespace :v1 do
-      resources :markets, :only => [:index, :show], :defaults => {:format => 'json'} do
+    namespace :v1, :defaults => {:format => 'json'} do
+      resources :markets, :only => [:index, :show] do
         resources :products, :only => [:index]
         resources :payment_types, :only => [:index]
         resources :schedules, :only => [:index]
