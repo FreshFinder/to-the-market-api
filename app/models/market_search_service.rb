9c8e4e3482bb871846c7bdd2b0b499bc937a3615
Simon Taranto <simon.taranto@gmail.com>
2014-02-12 15:04:09 -0700

    Add select

--- app/models/market_search_service.rb
+++ app/models/market_search_service.rb
@@ -1,7 +1,7 @@
 class MarketSearchService < ActiveRecord::Base
 
   def self.by_zipcode(target_zipcode, radius = 30)
-    market_ids = Address.near(target_zipcode.to_s, radius).map(&:market_id)
+    market_ids = Address.near(target_zipcode.to_s, radius).select("id").map(&:market_id)
     markets = Market.includes(:address).where(:id => market_ids)
   end
 
